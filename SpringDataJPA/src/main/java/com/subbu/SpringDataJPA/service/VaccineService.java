72d0a03fc9a5b4efbd7177f90bd817de7ed0fed1
subbuakula <subrahmanyamwebsites>
2024-10-10 11:34:07 +0530

    new methods are added in service layer

--- SpringDataJPA/src/main/java/com/subbu/SpringDataJPA/service/VaccineService.java
+++ SpringDataJPA/src/main/java/com/subbu/SpringDataJPA/service/VaccineService.java
@@ -1,10 +1,18 @@
 package com.subbu.SpringDataJPA.service;
 
+import java.util.List;
+
 import com.subbu.SpringDataJPA.model.Vaccine;
 
 public interface VaccineService 
 {
 	Iterable<Vaccine> getAllVaccines();
-	
+	Vaccine getVaccineById(int id);
+	Vaccine addVaccine(Vaccine v);
+	Iterable<Vaccine> addAllVaccines(Iterable<Vaccine> vaccines);
+    Iterable<Vaccine> getAllVaccinesById(Iterable<Integer> ids);
+    String deleteVaccineById(int id);
+    String deleteVaccinesByIds(Iterable<Integer> ids);
+    
 
 }
--- SpringDataJPA/src/main/java/com/subbu/SpringDataJPA/service/VaccineServiceImpl.java
+++ SpringDataJPA/src/main/java/com/subbu/SpringDataJPA/service/VaccineServiceImpl.java
@@ -1,5 +1,8 @@
 package com.subbu.SpringDataJPA.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,56 @@ public class VaccineServiceImpl implements VaccineService {
 	@Override
 	public Iterable<Vaccine> getAllVaccines() {
 		return vaccineDao.findAll();
+	}
+
+	@Override
+	public Vaccine getVaccineById(int id) {
+		return vaccineDao.findById(id).get();
+	}
+
+	@Override
+	public Vaccine addVaccine(Vaccine v) {
+		return vaccineDao.save(v);
+	}
+
+	@Override
+	public Iterable<Vaccine> addAllVaccines(Iterable<Vaccine> vaccines) {
+		return vaccineDao.saveAll(vaccines);
+	}
+
+	@Override
+	public Iterable<Vaccine> getAllVaccinesById(Iterable<Integer> ids) {
+		 List<Vaccine> vaccines = (List<Vaccine>) vaccineDao.findAllById(ids);
+//		 int size1 = ((List<Integer>) ids).size();
+		 if(vaccines.size() == ((List<Integer>)ids).size())
+		 {
+			 return vaccines;
+		 }
+		 return new ArrayList<Vaccine>();
+	}
+
+	@Override
+	public String deleteVaccineById(int id) 
+	{
+		Vaccine v = vaccineDao.findById(id).get();
+		if(v!=null)
+		{
+			vaccineDao.deleteById(id);
+			return "Vaccine with given Id is deleted.";
+		}
+				
+		 return "Vaccine with given Id is not available";
+		 
+	}
+
+	@Override
+	public String deleteVaccinesByIds(Iterable<Integer> ids) {
+		if(((List<Vaccine>)vaccineDao.findAllById(ids)).size() == ((List<Integer>)ids).size())
+		{
+			vaccineDao.deleteAllById(ids);
+			return "vaccines with all given ids are deleted.";
+		}
+		return "Vaccines with all given Ids are not available";
 	}
 
 }
